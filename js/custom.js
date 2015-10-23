a70da4bd174f48f0e61890b6a65090d73ff22ffa
NamNguyen52 <nam.nguyen52@gmail.com>
2015-10-22 20:01:44 -0700

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.testimonialSlider').slick({
+		dots: false,
+		infinite: true,
+		speed: 500,
+		autoplay:true,
+		autoplaySpeed: 3000,
+		arrows:false,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+});
